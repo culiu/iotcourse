d59f6cfe5f4515a87e389280753c496265fb24ce
unknown <1115493128@qq.com>
2022-09-19 21:32:59 +0800

    addition

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include<iostream>
 using namespace std;
-
+int count(int a)
+{
+    for(int i=0;i<a;i++){
+	    int sum=0;
+	    sum+=a;
+    }
 int main()
 {
-    cout << "hello, world" << endl;
+    
+    cout << count(10) << endl;
     return 0;
 } 

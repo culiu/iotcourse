d0b77be55d83ee3eceafe58ec388dd726f1cc30e
“Hum8le” <1115493128@qq.com>
2022-09-19 23:34:51 +0800

    final test

--- main.cpp
+++ main.cpp
@@ -14,16 +14,4 @@ int main()
     cout << count(10) << endl;
     return 0;
 } 
-#include<iostream>
-using namespace std;
-int sum(){
-    int sum=0;
-  for(i=1;i<=20;i++) sum=sum+i;
-  return sum;
-}
-int main()
-{
-    cout << "hello, world" << endl;
-    return 0;
-} 
 >>>>>>> a28b72716af69ed610e9da00016a9817c54cfa51

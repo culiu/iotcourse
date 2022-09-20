bf9154d2d2518087fa1efe0d5d1189358fd820b0
“Hum8le” <1115493128@qq.com>
2022-09-20 11:03:06 +0800

    vscode push

--- main.cpp
+++ main.cpp
@@ -1,17 +1,15 @@
-<<<<<<< HEAD
 #include<iostream>
 using namespace std;
 int count(int a)
-{
-    for(int i=0;i<a;i++){
-	    int sum=0;
-	    sum+=a;
-    }
+{   
+    int sum=0;
+    for(int i=0;i<a;i++)sum+=i;
+    return sum;
 }
+
 int main()
 {
     
     cout << count(10) << endl;
     return 0;
 } 
->>>>>>> a28b72716af69ed610e9da00016a9817c54cfa51
